84193bc3ed9bc315ee0583360b8b5cf09be57cae
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-06-21 15:42:57 -0300

    feat: add components user, search and error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Home from "./routes/Home"
+import classes from "./App.module.css"
 
 function App() {
 
   return (
-    <div className='app'>
+    <div className={classes.app}>
       <h1>GitHub Finder</h1>
       <Home />
     </div>
