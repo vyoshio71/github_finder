bcbfc012923156ccbeb375e8cb3f8fe8be184e65
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-06-21 14:26:07 -0300

    create initial project and routes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./routes/Home"
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>GitHub Finder</h1>
+      <Home />
+    </div>
+  )
+}
+
+export default App
